4f3759ebcb7d87d2a370660ae79862bd3f823f84
Leonardo Costa <112534103+Justice3001@users.noreply.github.com>
2023-08-16 23:02:03 -0400

    Add files via upload

---
+++ secondland.js
@@ -0,0 +1,46 @@
+/* Please ❤ this if you like it! */
+
+(function ($) {
+    "use strict";
+
+    // Page cursors
+    // ... (cursor-related code)
+
+    // Navigation
+    var app = function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function () {
+                return toggleClass(body, 'nav-active');
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass);
+            else element.classList.add(stringClass);
+        };
+        init();
+    }();
+
+    
+
+    // Dropdown menu functionality
+    var menuDropdown = document.querySelector('.menu-icon');
+    var dropdownMenu = document.querySelector('.nav__list-dropdown');
+    menuDropdown.addEventListener('click', function () {
+        toggleClass(dropdownMenu, 'dropdown-active');
+    });
+
+    function toggleClass(element, stringClass) {
+        if (element.classList.contains(stringClass)) element.classList.remove(stringClass);
+        else element.classList.add(stringClass);
+    }
+
+})(jQuery);
