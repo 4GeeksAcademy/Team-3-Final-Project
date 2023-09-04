8252bed0d13bb93022c2ba511b376647917fd58b
Leonardo Costa <112534103+Justice3001@users.noreply.github.com>
2023-09-04 01:22:10 +0000

    things changed

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -16,4 +16,25 @@ export const Home = () => {
 
 		</div>
 	);
+    useEffect(() => {
+        // Check if the access token is present in the URL fragment
+        const urlParams = new URLSearchParams(window.location.hash.slice(1));
+        const accessToken = urlParams.get("access_token");
+
+        if (accessToken) {
+            // Now you can use the access token for API requests
+            console.log("Access Token:", accessToken);
+            // You can also save it to your app's state/context if needed
+            actions.setAccessToken(accessToken); // Replace with your action function
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Log in with Spotify</h1>
+            <a href="https://accounts.spotify.com/authorize?response_type=token&client_id=5aa86719c259487c82565468bbaa1d2e&scope=comma-separated-list-of-scopes&redirect_uri=https://cautious-space-chainsaw-7vpgvqpv4q3pwwq-3001.app.github.dev/">
+                <button>Login with Spotify</button>
+            </a>
+        </div>
+    );
 };
