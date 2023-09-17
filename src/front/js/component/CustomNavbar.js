65ad742bf5a6a543a87027a6fe7330a46d857d4c
Jeankory V <124745650+jeankory500@users.noreply.github.com>
2023-09-17 16:16:53 -0400

    Update CustomNavbar.js

--- src/front/js/component/CustomNavbar.js
+++ src/front/js/component/CustomNavbar.js
@@ -53,9 +53,10 @@ const CustomNavbar = ({ history }) => {
           <NavbarBS.Toggle aria-controls="navbarScroll" />
           <NavbarBS.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Top Artists</Nav.Link>
-              <Nav.Link href="#action2">Top Albums</Nav.Link>
+              <Nav.Link href='/LandingPage'>Home</Nav.Link>
+              <Nav.Link href="/TopArtists">Top Artists</Nav.Link>
+              <Nav.Link href="/TopAlbums">Top Albums</Nav.Link>
+              <Nav.Link href="/ArtPage">Artists Search</Nav.Link>
               <NavDropdown title="Link" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
